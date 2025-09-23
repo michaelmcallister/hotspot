224a0898d2b1953a699f1552f93f1e5b9fd54d17
Michael McAllister <git@sknk.ws>
2025-09-23 20:15:26 +0200

    Commit logo

---
+++ src/web/src/components/Logo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="logo">
+    <v-icon class="mr-1" :size="size" :class="iconClass">mdi-motorbike</v-icon>
+    <span :class="textClass">Hotspot</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  size?: string | number
+  textClass?: string
+  iconClass?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: 'default',
+  textClass: '',
+  iconClass: ''
+})
+</script>
+
+<style scoped>
+.logo {
+  display: inline-flex;
+  align-items: center;
+}
+</style>
