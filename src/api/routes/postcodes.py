9866403d998d36f4a7543491b510e192bce0c12b
Michael McAllister <git@sknk.ws>
2025-09-29 23:48:23 +0200

    Simplify ParkingFeed

--- src/api/routes/postcodes.py
+++ src/api/routes/postcodes.py
@@ -19,13 +19,21 @@ def get_nearest_postcodes(request: Request, postcode: str) -> List[Dict[str, Any
             pd.secondary_postcode as postcode,
             pr.locality as suburb,
             pr.local_government_area as lga,
-            pd.distance_meters as distance_in_meters
+            pd.distance_meters as distance_in_meters,
+            pr.postcode_risk as risk_score
         FROM postcode_distances pd
         JOIN postcode_risk pr ON pd.secondary_postcode = pr.postcode
         WHERE pd.primary_postcode = :postcode
         ORDER BY pd.distance_meters ASC
         LIMIT 20
     """, {"postcode": postcode})
+
+    for postcode_data in nearest_postcodes:
+        try:
+            parking_data = db.get_parking_by_postcode(postcode_data['postcode'])
+            postcode_data['parking_count'] = len(parking_data) if parking_data else 0
+        except:
+            postcode_data['parking_count'] = 0
 
     return nearest_postcodes
 
