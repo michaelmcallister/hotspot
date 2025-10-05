9e61f9938e24293f37b291a76f727a008090405b
Minhal <mzeh0001@student.monash.edu>
2025-10-05 12:04:46 +1030

    Updated FAQ page and removed FAQ access from overflow menu

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -21,9 +21,6 @@
             <v-list-item @click="showResources = true">
               <v-list-item-title class="text-button text-primary">Resources</v-list-item-title>
             </v-list-item>
-            <v-list-item :to="{ name: 'faq' }">
-              <v-list-item-title class="text-button text-primary">FAQ</v-list-item-title>
-            </v-list-item>
             <v-list-item to="/settings">
               <v-list-item-title class="text-button text-primary">Settings</v-list-item-title>
             </v-list-item>
